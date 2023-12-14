1569e1e31e6d589723ac6db554862e301786f637
Your Name <you@example.com>
2023-12-14 23:45:56 +0700

    Complete Auth Pages

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div>
+            <img src="/assets/icons/loader.svg" alt="loader" width={22} height={22} />
+        </div>
+    );
+};
+
+export default Loader;
