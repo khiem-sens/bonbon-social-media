d90fde253d2c1b1c0d2dcc7f9900cfe641777e54
Your Name <you@example.com>
2024-01-05 21:53:06 +0700

    Explore fixed

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -44,7 +44,8 @@ const Explore = () => {
         );
 
     const shouldShowSearchResults = searchValue !== '';
-    const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item.documents.length === 0);
+    const shouldShowPosts =
+        !shouldShowSearchResults && posts.pages.every((item) => item && item.documents.length === 0);
 
     return (
         <div className="explore-container">
@@ -80,7 +81,9 @@ const Explore = () => {
                 ) : shouldShowPosts ? (
                     <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
                 ) : (
-                    posts.pages.map((item, index) => <GridPostList key={`page-${index}`} posts={item.documents} />)
+                    posts.pages.map(
+                        (item, index) => item && <GridPostList key={`page-${index}`} posts={item.documents} />,
+                    )
                 )}
             </div>
 
