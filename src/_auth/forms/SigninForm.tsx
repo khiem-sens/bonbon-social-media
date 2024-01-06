cb8d4b91c53bf9ba7e0db56247822b0a12a385e5
Your Name <you@example.com>
2024-01-06 09:11:34 +0700

    Responsive sign in ui

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -53,7 +53,7 @@ const SigninForm = () => {
 
     return (
         <Form {...form}>
-            <div className="sm:w-420 flex-center flex-col">
+            <div className="sm:w-420 px-6 flex-center flex-col">
                 <img src="/assets/images/logo.png" alt="logo" />
 
                 <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Log in to your account</h2>
