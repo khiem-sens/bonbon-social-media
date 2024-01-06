d51ecfdc9bc6ae88ae7ea5ff3a0c9a60902bdd91
Your Name <you@example.com>
2024-01-06 09:40:30 +0700

    1

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -79,7 +79,7 @@ const SignupForm = () => {
 
                 <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
                 <p className="text-light-3 small-medium md:base-regular mt-2">
-                    To use snapgram, Please enter your details
+                    To use Bonbon, Please enter your details
                 </p>
 
                 <form onSubmit={form.handleSubmit(handleSignup)} className="flex flex-col gap-5 w-full mt-4">
